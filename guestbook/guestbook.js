184701326edf2997fdc90c1d8311d8b7f2cb1d07
AlexABallWebDev <aball6@mail.greenriver.edu>
2016-01-28 11:14:28 -0800

    added submit button. messages are saved to a MongoDB collection.

--- guestbook/guestbook.js
+++ guestbook/guestbook.js
@@ -1,19 +1,40 @@
+Messages = new Mongo.Collection('messages');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	//event listeners for the guestBook template
+  Template.guestBook.events({
+		//when the form is submitted, execute this block of code.
+		"submit form": function(event) {
+			//prevent the default behavior, which would submit the
+			//form back to this page.
+			event.preventDefault();
+			
+			//one way to get the messagebox from the form.
+			//var messageBox = document.getElementById("example");
+			
+			//save the messageBox as an object into a variable.
+			var messageBox =
+						$(event.target).find('textarea[name=guestBookMessage]');
+			
+			//save the text from the messageBox object into a variable.
+			var messageText = messageBox.val();
+			
+			//save the name as an object into a variable.
+			var nameBox =
+						$(event.target).find('input[name=guestName]');
+			
+			//save the text from the name object into a variable.
+			var nameText = nameBox.val();
+			
+			//insert an entry into the messages collection
+			Messages.insert({
+				"name": nameText,
+				"message": messageText
+			});
+			
+			alert("name: " + nameText + " message: " + messageText);
+		}
+	});
 }
 
 if (Meteor.isServer) {
