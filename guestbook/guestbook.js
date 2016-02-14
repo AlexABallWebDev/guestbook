ff38d0f4ed8b9c5556c508e55bbde32520dd6c7e
AlexABallWebDev <aball6@mail.greenriver.edu>
2016-02-13 20:33:33 -0800

    Added users and authentication. Users must be logged in to add new messages. Fixed the display of the message date/time so that a user can read it.

--- guestbook/guestbook.js
+++ guestbook/guestbook.js
@@ -26,16 +26,24 @@ Router.route('/messages/:_id', function() {
 if (Meteor.isClient) {
 	//subscribe to get the Messages collection.
 	Meteor.subscribe("messages");
-	
-	
+
+
 	Template.guestBook.helpers({
 		"messages": function() {
 			//return all message objects, or an empty object if DB
 			//is invalid. sort with most recent entries first.
 			return Messages.find({}, {sort: {createdOn: -1}}) || {};
+		},
+		"convertDate": function(date) {
+			//convert the number to a user-friendly (legible) date and time.
+			var convertedDate = new Date(date);
+
+			return ((convertedDate.getMonth() + 1) + '/' + convertedDate.getDate() +
+						'/' + convertedDate.getFullYear() + ' ' + convertedDate.getHours() +
+						':' + convertedDate.getMinutes());
 		}
 	});
-	
+
 	//event listeners for the guestBook template
   Template.guestBook.events({
 		//when the form is submitted, execute this block of code.
@@ -43,38 +51,38 @@ if (Meteor.isClient) {
 			//prevent the default behavior, which would submit the
 			//form back to this page.
 			event.preventDefault();
-			
+
 			//one way to get the messagebox from the form.
 			//var messageBox = document.getElementById("example");
-			
+
 			//save the messageBox as an object into a variable.
 			var messageBox =
 						$(event.target).find('textarea[name=guestBookMessage]');
-			
+
 			//save the text from the messageBox object into a variable.
 			var messageText = messageBox.val();
-			
+
 			//save the name as an object into a variable.
-			var nameBox =
-						$(event.target).find('input[name=guestName]');
-			
+			/*var nameBox =
+						$(event.target).find('input[name=guestName]');*/
+
 			//save the text from the name object into a variable.
-			var nameText = nameBox.val();
-			
+			//var nameText = nameBox.val();
+
 			//if name and message have data,
 			//enter it into the messages collection.
-			if (nameText.length > 0 &&
+			if (Accounts.user() != null &&
 					messageText.length > 0) {
-			
+
 				//insert an entry into the messages collection
 				Messages.insert({
-					"name": nameText,
+					"name": Accounts.user().username,
 					"message": messageText,
 					"createdOn": Date.now()
 				});
-			
+
 				//clear the inputs after an entry is added.
-				nameBox.val("");
+				//nameBox.val("");
 				messageBox.val("");
 			}
 			//display an error when one of the boxes is empty.
@@ -83,13 +91,18 @@ if (Meteor.isClient) {
 			}
 		}
 	});
+
+	Accounts.ui.config({
+		//only usernames are used in guestbook.
+		passwordSignupFields: "USERNAME_ONLY"
+	});
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-	
+
 	//publish the messages collection.
 	Meteor.publish("messages", function () {
 		return Messages.find();
